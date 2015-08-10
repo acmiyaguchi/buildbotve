c27c90129da79fff0d7934450cc678e94b522c52
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-08-10 14:19:41 -0700

    Initial commit

---
+++ scripts/easy_install-script.py
@@ -0,0 +1,10 @@
+#!c:\mozilla-build\buildbotve\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'distribute==0.6.14','console_scripts','easy_install'
+__requires__ = 'distribute==0.6.14'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('distribute==0.6.14', 'console_scripts', 'easy_install')()
+    )
